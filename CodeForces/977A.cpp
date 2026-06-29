08fdd8b5a4d2737b6899f384659665c95feb0138
Arpan-me-ios <arpankumar1009@gmail.com>
2026-06-29 17:05:46 +0530

    Solve : 977A - Wrong Subtraction

---
+++ CodeForces/977A.cpp
@@ -0,0 +1,25 @@
+// 4A.cpp
+
+// This file is part of CodeForces
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m;
+    int n;
+    cin >> m;
+    cin >> n;
+    int i=0;
+    while(i<n){
+        if(m%10==0){
+            m=m/10;
+        }
+        else{
+            m-=1;
+        }
+        i+=1;
+    }
+    cout << m;
+    return 0;
+}
