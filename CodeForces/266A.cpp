49ddad4db3668a8e452aa8114dc08271e83d251a
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-02 00:04:21 +0530

    Solve : 266A - Stones on the Table

--- CodeForces/266A.cpp
+++ CodeForces/266A.cpp
@@ -1,24 +1,25 @@
-// 467A.cpp
+// 266A.cpp
 
 // This file is part of CodeForces
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     string a;
     cin >> n;
     cin >> a;
     int count = 0;
-    int i=0;
-    for(i=0;i<n;i+=1){
-        if(a[i]==a[i+1]){
-            count+=1
+    int i = 0;
+    for (i = 0; i < n; i += 1)
+    {
+        if (a[i] == a[i + 1])
+        {
+            count += 1;
         }
     }
     cout << count;
-    return 0
+    return 0;
 }
-
-    
