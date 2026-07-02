17500e3e2dc9356cc806c352c1667aadb2ce2b21
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-02 23:42:27 +0530

    Solve : 707 A

---
+++ CodeForces/2241B.cpp
@@ -0,0 +1,17 @@
+// 1030A.cpp
+
+// This file is part of CodeForces
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+    for(int i=0;i<n;i+=1){
+        cin >> m;
+        for(int k=0; k<99999; k+=1){
+            if // k is good && k*m is good
+        }
+
+    }
---
+++ CodeForces/707A.cpp
@@ -0,0 +1,36 @@
+// 617A.cpp
+
+// This file is part of CodeForces
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+    cin >> n;
+    cin >> m;
+    int j;
+    int i;
+    string a;
+    string array[m][n];
+    for (i = 0; i < m; i += 1)
+    {
+        for (j = 0; j < n; j += 1)
+        {
+            cin >> array[i][j];
+            a = array[i][j];
+            if (a == "B" || a == "W" || a == "G")
+            {
+                continue;
+            }
+            else
+            {
+                cout << "#Color";
+                return 0;
+            }
+        }
+    }
+    cout << "#Black&White";
+}
