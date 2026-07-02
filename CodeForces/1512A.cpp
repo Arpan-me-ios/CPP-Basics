95a77296182f8c8498d2ba0459623312b5b2b3d1
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-02 18:42:35 +0530

    Extra

---
+++ CodeForces/1512A.cpp
@@ -0,0 +1,25 @@
+// 1030A.cpp
+
+// This file is part of CodeForces
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    for(int i=0;i<n;i+=1){
+        int m;
+        cin >> m;
+        int ar[m];
+        for(int j=0;j<m;j+=1){
+            cin >> ar[j];
+
+        }
+        for(int k=0; k<m; k+=1){
+            if (ar[k]!=ar[k+1] && ar[k]==ar[k+2]){
+                cout << (k+2) << endl;
+            }
+        }
+    }
+}
