bcebb79eaba74bf0821abbebfdd09bbcf753e701
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-08 21:15:49 +0530

    Solve : 2227A - Koshary

---
+++ CodeForces/2227A.cpp
@@ -0,0 +1,26 @@
+// 241A.cpp
+
+// This file is part of CodeForces
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i += 1)
+    {
+        int a;
+        int b;
+        cin >> a >> b;
+        if (a % 2 == 0 || b % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
