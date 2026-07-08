56e4aa5402e3fb103e9687aa7f4f35265379440e
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-08 22:20:04 +0530

    Solve : 2148A - Sublime Sequence

---
+++ CodeForces/2148A.cpp
@@ -0,0 +1,26 @@
+// 2248A.cpp
+
+// This file is part of CodeForces
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i += 1)
+    {
+        int a;
+        int b;
+        cin >> a >> b;
+        if (b % 2 == 0)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << a << endl;
+        }
+    }
+}
