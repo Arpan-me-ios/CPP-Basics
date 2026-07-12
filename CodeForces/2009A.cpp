d63b585de5a1d3cef810bc0c5169c1d2f86ed16c
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-12 21:00:32 +0530

    Solve: 2009A

---
+++ CodeForces/2009A.cpp
@@ -0,0 +1,18 @@
+// 2009A.cpp
+
+// This file is part of CodeForces
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i += 1){
+        int a;
+        int b;
+        cin>>a>>b;
+        cout << b-a <<endl;
+    }
+}
