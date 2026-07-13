f69f7aef28257ee874fdc6f602e0bb201e06e09a
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-13 22:50:04 +0530

    Solve : 2094A - Trippi Troppi

---
+++ CodeForces/2094A.cpp
@@ -0,0 +1,19 @@
+// 2094A.cpp
+
+// This file is part of CodeForces
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string a;
+    string b;
+    string c;
+    for (int i = 0; i < n; i += 1){
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+}
