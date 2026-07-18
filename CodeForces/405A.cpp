3387c8b80d4fed9158bde390b28732466d31a902
Arpan-me-ios <arpankumar1009@gmail.com>
2026-07-18 23:33:24 +0530

    Solve : 405A - Gravity Flip

--- CodeForces/405A.cpp
+++ CodeForces/405A.cpp
@@ -2,7 +2,24 @@
 
 // This file is part of CodeForces
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
+    vector<int> v1;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int m;
+        cin >> m;
+        v1.push_back(m);
+    }
+
+    sort(v1.begin(), v1.end());
+    for (int j = 0; j < n; j++)
+    {
+        cout << v1[j] << " ";
+    }
+}
